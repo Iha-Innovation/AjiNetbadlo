878d8df3173dad60b5043bc0ad86899c67d41fef
Iha-Innovation <iha.innovation@gmail.com>
2019-09-18 18:12:45 +0200

    first commit

--- rehomes/rehomes/index.php
+++ rehomes/rehomes/index.php
@@ -1,171 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Title -->
-    <title>Rehomes - Real Estate HTML Template</title>
-
-    <!-- Favicon -->
-    <link rel="icon" href="./img/core-img/favicon.png">
-
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="style.css">
-
-</head>
-
-<body>
-    <!-- Preloader -->
-    <div id="preloader">
-        <div class="loader"></div>
-    </div>
-
-    <!-- **** Header Area Start **** -->
-    <header class="header-area">
-        <!-- Top Header Area Start -->
-        <div class="top-header-area">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-6">
-                        <div class="top-header-content">
-                            <a href="#"><i class="icon_mail"></i> <span>contact@ihainnovation.com</span></a>
-                            <a href="#"><i class="icon_phone"></i> <span>(+212) 606</span></a>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="top-header-content">
-                            <!-- Top Social Area -->
-                            <div class="top-social-area ml-auto">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Top Header Area End -->
-
-        <!-- Main Header Start -->
-        <div class="main-header-area animated">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
-                    <!-- Classy Menu -->
-                    <nav class="classy-navbar justify-content-between" id="rehomesNav">
-
-                        <!-- Logo -->
-                        <a class="nav-brand" href="./index.html"><img src="./img/core-img/logo.png" alt=""></a>
-
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-
-                        <!-- Menu -->
-                        <div class="classy-menu">
-                            <!-- Menu Close Button -->
-                            <div class="classycloseIcon">
-                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                            </div>
-                            <!-- Nav Start -->
-                            <div class="classynav">
-                                <ul id="nav">
-                                    <li class="active"><a href="./index.html">Home</a></li>
-                                    <li><a href="#">Pages</a>
-                                        <ul class="dropdown">
-                                            <li class="active"><a href="./index.html">- Home</a></li>
-                                            <li><a href="./about.html">- About</a></li>
-                                            <li><a href="./agent.html">- Agent</a></li>
-                                            <li><a href="./rent.html">- Rent</a></li>
-                                            <li><a href="./properties-details.html">- Property Details</a></li>
-                                            <li><a href="./blog.html">- Blog</a></li>
-                                            <li><a href="./single-blog.html">- Single Blog</a></li>
-                                            <li><a href="./contact.html">- Contact</a></li>
-                                            <li><a href="#">- Dropdown</a>
-                                                <ul class="dropdown">
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="./rent.html">Rent</a></li>
-                                    <li><a href="./agent.html">Agents</a></li>
-                                    <li><a href="./about.html">About</a></li>
-                                    <li><a href="./blog.html">Blog</a></li>
-                                </ul>
-
-                                <!-- Contact btn -->
-                                <div class="contact-btn mt-3 mt-lg-0 ml-3 ml-lg-5">
-                                    <a href="#">Contact Us</a>
-                                </div>
-                            </div>
-                            <!-- Nav End -->
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- **** Header Area End **** -->
-
+<?php
+include("header.html");
+?>
     <!-- **** Welcome Maps Area Start **** -->
-    <div class="site-blocks-cover overlay" style="background-image: url(images/hero_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="breadcrumb-area bg-img bg-overlay-3 jarallax wow fadeInUp" data-wow-delay="200ms" style="background-image: url(img/bg-img/30.jpg);">
+
+    
         <div class="container">
           <div class="row align-items-center justify-content-center text-center">
   
-            <div class="col-md-12">
+            <div class="col-md-12" style="top: 41px;">
               
               
               <div class="row justify-content-center mb-4">
                 <div class="col-md-8 text-center">
+               
                   <h1 class="" data-aos="fade-up">Échanger votre maison n'as jamais été aussi simple. </h1>
-                  <p data-aos="fade-up" data-aos-delay="100">You can buy, sell anything you want.</p>
+
                 </div>
               </div>
   
-              <div class="form-search-wrap" data-aos="fade-up" data-aos-delay="200">
-                <form method="post">
-                  <div class="row align-items-center">
-                    <div class="col-lg-12 mb-4 mb-xl-0 col-xl-4">
-                      <input type="text" class="form-control rounded" placeholder="What are you looking for?">
-                    </div>
-                    <div class="col-lg-12 mb-4 mb-xl-0 col-xl-3">
-                      <div class="wrap-icon">
-                        <span class="icon icon-room"></span>
-                        <input type="text" class="form-control rounded" placeholder="Location">
-                      </div>
-                      
-                    </div>
-                    <div class="col-lg-12 mb-4 mb-xl-0 col-xl-3">
-                      <div class="select-wrap">
-                        <span class="icon"><span class="icon-keyboard_arrow_down"></span></span>
-                        <select class="form-control rounded" name="" id="">
-                          <option value="">All Categories</option>
-                          <option value="">Real Estate</option>
-                          <option value="">Books &amp;  Magazines</option>
-                          <option value="">Furniture</option>
-                          <option value="">Electronics</option>
-                          <option value="">Cars &amp; Vehicles</option>
-                          <option value="">Others</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div class="col-lg-12 col-xl-2 ml-auto text-right">
-                      <input type="submit" class="btn btn-primary btn-block rounded" value="Search">
-                    </div>
-                    
-                  </div>
-                </form>
-              </div>
+              
   
             </div>
           </div>
