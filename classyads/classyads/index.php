3406662de9afd3bee1036a60233d4f2f30d9d5df
Iha-Innovation <iha.innovation@gmail.com>
2019-09-18 17:28:11 +0200

    first commit

--- classyads/classyads/index.php
+++ classyads/classyads/index.php
@@ -1,77 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>ClassyAds &mdash; Colorlib Website Template</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/rangeslider.css">
-
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body>
-  
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-    
-    <header class="site-navbar container py-0 bg-white" role="banner">
-
-      <!-- <div class="container"> -->
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black mb-0">Aji<span class="text-primary">Netbadlo</span>  </a></h1>
-          </div>
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-
-              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li class="active"><a href="index.html">Acceuil</a></li>
-               
-                
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="contact.html">Contact</a></li>
-
-                <li class="ml-xl-3 login"><a href="login.html"><span class="border-left pl-xl-4"></span>S'identifier</a></li>
-                <li><a href="register.html">S'inscrire</a></li>
-
-                <li><a href="#" class="cta"><span class="bg-primary text-white rounded">+ Post an Ad</span></a></li>
-              </ul>
-            </nav>
-          </div>
-
-
-          <div class="d-inline-block d-xl-none ml-auto py-3 col-6 text-right" style="position: relative; top: 3px;">
-            <a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a>
-          </div>
-
-        </div>
-      <!-- </div> -->
-      
-    </header>
+<?php
+include("header.html");
+?>
 
   
 
@@ -84,7 +13,7 @@
             
             <div class="row justify-content-center mb-4">
               <div class="col-md-8 text-center">
-                <h1 class="" data-aos="fade-up">Echanger votre maison n'as jamais été aussi simple. </h1>
+                <h1 class="" data-aos="fade-up">Échanger votre maison n'as jamais été aussi simple. </h1>
                 <p data-aos="fade-up" data-aos-delay="100">You can buy, sell anything you want.</p>
               </div>
             </div>
