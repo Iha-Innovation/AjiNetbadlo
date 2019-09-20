01d38bcc51c4296423bf547b8048c3fd44c2e1df
iha-innovation <iha-innovation@github.com>
2019-09-20 11:56:51 -0700

    acceuil temp1

--- classyads/classyads/index.php
+++ classyads/classyads/index.php
@@ -379,138 +379,14 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
     </div>
 
 
-    <div class="site-section bg-light">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-7 text-left border-primary">
-            <h2 class="font-weight-light text-primary">Catégorie</h2>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-lg-6">
-
-            <div class="d-block d-md-flex listing">
-              <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_2.jpg')"></a>
-              <div class="lh-content">
-                <span class="category">Real Estate</span>
-                <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                <h3><a href="listings-single.html">House with Swimming Pool</a></h3>
-                <address>Don St, Brooklyn, New York</address>
-                <p class="mb-0">
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-secondary"></span>
-                  <span class="review">(3 Reviews)</span>
-                </p>
-              </div>
-            </div>
-            <div class="d-block d-md-flex listing">
-                <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_3.jpg')"></a>
-                <div class="lh-content">
-                  <span class="category">Furniture</span>
-                  <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                  <h3><a href="listings-single.html">Wooden Chair &amp; Table</a></h3>
-                  <address>Don St, Brooklyn, New York</address>
-                  <p class="mb-0">
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-secondary"></span>
-                    <span class="review">(3 Reviews)</span>
-                  </p>
-                </div>
-              </div>
-
-              <div class="d-block d-md-flex listing">
-                <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_4.jpg')"></a>
-                <div class="lh-content">
-                  <span class="category">Electronics</span>
-                  <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                  <h3><a href="listings-single.html">iPhone X gray</a></h3>
-                  <address>Don St, Brooklyn, New York</address>
-                  <p class="mb-0">
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-secondary"></span>
-                    <span class="review">(3 Reviews)</span>
-                  </p>
-                </div>
-              </div>
-
-             
-
-          </div>
-          <div class="col-lg-6">
-
-            <div class="d-block d-md-flex listing">
-              <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_1.jpg')"></a>
-              <div class="lh-content">
-                <span class="category">Cars &amp; Vehicles</span>
-                <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                <h3><a href="listings-single.html">Red Luxury Car</a></h3>
-                <address>Don St, Brooklyn, New York</address>
-                <p class="mb-0">
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-secondary"></span>
-                  <span class="review">(3 Reviews)</span>
-                </p>
-              </div>
-            </div>
-
-            <div class="d-block d-md-flex listing">
-              <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_2.jpg')"></a>
-              <div class="lh-content">
-                <span class="category">Real Estate</span>
-                <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                <h3><a href="listings-single.html">House with Swimming Pool</a></h3>
-                <address>Don St, Brooklyn, New York</address>
-                <p class="mb-0">
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-warning"></span>
-                  <span class="icon-star text-secondary"></span>
-                  <span class="review">(3 Reviews)</span>
-                </p>
-              </div>
-            </div>
-            <div class="d-block d-md-flex listing">
-                <a href="listings-single.html" class="img d-block" style="background-image: url('images/img_3.jpg')"></a>
-                <div class="lh-content">
-                  <span class="category">Furniture</span>
-                  <a href="#" class="bookmark"><span class="icon-heart"></span></a>
-                  <h3><a href="listings-single.html">Wooden Chair &amp; Table</a></h3>
-                  <address>Don St, Brooklyn, New York</address>
-                  <p class="mb-0">
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-warning"></span>
-                    <span class="icon-star text-secondary"></span>
-                    <span class="review">(3 Reviews)</span>
-                  </p>
-                </div>
-              </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
+   
     
     <div class="site-section bg-white">
       <div class="container">
 
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Testimonials</h2>
+            <h2 class="font-weight-light text-primary">Opinion concernant l'échange de maison.</h2>
           </div>
         </div>
 
@@ -522,7 +398,9 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
                 <p>John Smith</p>
               </figure>
               <blockquote>
-                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+                <p>&ldquo;Nous avons passé un agréable séjour d'une dizaine de jours à El Jadida.<br>
+                Plage manifique, ville calme, footing, c'est une agréable ville.
+                &rdquo;</p>
               </blockquote>
             </div>
           </div>
@@ -533,8 +411,9 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
                 <p>Christine Aguilar</p>
               </figure>
               <blockquote>
-                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-              </blockquote>
+              <p>&ldquo;Nous avons passé un agréable séjour d'une dizaine de jours à El Jadida.<br>
+                Plage manifique, ville calme, footing, c'est une agréable ville.
+                &rdquo;</p> </blockquote>
             </div>
           </div>
 
@@ -545,8 +424,9 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
                 <p>Robert Spears</p>
               </figure>
               <blockquote>
-                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-              </blockquote>
+              <p>&ldquo;Nous avons passé un agréable séjour d'une dizaine de jours à El Jadida.<br>
+                Plage manifique, ville calme, footing, c'est une agréable ville.
+                &rdquo;</p></blockquote>
             </div>
           </div>
 
@@ -557,8 +437,9 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
                 <p>Bruce Rogers</p>
               </figure>
               <blockquote>
-                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-              </blockquote>
+              <p>&ldquo;Nous avons passé un agréable séjour d'une dizaine de jours à El Jadida.<br>
+                Plage manifique, ville calme, footing, c'est une agréable ville.
+                &rdquo;</p></blockquote>
             </div>
           </div>
 
