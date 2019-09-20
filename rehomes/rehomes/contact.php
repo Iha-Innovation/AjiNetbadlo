23cb507a4f35b29b0cf1226a8fcfb8487d2c2083
Iha-Innovation <iha.innovation@gmail.com>
2019-09-20 21:58:40 +0200

    AjiNetbadlo2

--- rehomes/rehomes/contact.php
+++ rehomes/rehomes/contact.php
@@ -1,120 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Title -->
-    <title>Rehomes - Real Estate HTML Template</title>
-
-    <!-- Favicon -->
-    <link rel="icon" href="./img/core-img/favicon.png">
-
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="style.css">
-
-</head>
-
-<body>
-    <!-- Preloader -->
-    <div id="preloader">
-        <div class="loader"></div>
-    </div>
-
-    <!-- **** Header Area Start **** -->
-    <header class="header-area">
-        <!-- Top Header Area Start -->
-        <div class="top-header-area">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-6">
-                        <div class="top-header-content">
-                            <a href="#"><i class="icon_mail"></i> <span>info.colorlib@gmail.com</span></a>
-                            <a href="#"><i class="icon_phone"></i> <span>(12) 345 6789</span></a>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="top-header-content">
-                            <!-- Top Social Area -->
-                            <div class="top-social-area ml-auto">
-                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Top Header Area End -->
-
-        <!-- Main Header Start -->
-        <div class="main-header-area animated">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
-                    <!-- Classy Menu -->
-                    <nav class="classy-navbar justify-content-between" id="rehomesNav">
-
-                        <!-- Logo -->
-                        <a class="nav-brand" href="./index.html"><img src="./img/core-img/logo.png" alt=""></a>
-
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-
-                        <!-- Menu -->
-                        <div class="classy-menu">
-                            <!-- Menu Close Button -->
-                            <div class="classycloseIcon">
-                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                            </div>
-                            <!-- Nav Start -->
-                            <div class="classynav">
-                                <ul id="nav">
-                                    <li class="active"><a href="./index.html">Home</a></li>
-                                    <li><a href="#">Pages</a>
-                                        <ul class="dropdown">
-                                            <li class="active"><a href="./index.html">- Home</a></li>
-                                            <li><a href="./about.html">- About</a></li>
-                                            <li><a href="./agent.html">- Agent</a></li>
-                                            <li><a href="./rent.html">- Rent</a></li>
-                                            <li><a href="./properties-details.html">- Property Details</a></li>
-                                            <li><a href="./blog.html">- Blog</a></li>
-                                            <li><a href="./single-blog.html">- Single Blog</a></li>
-                                            <li><a href="./contact.html">- Contact</a></li>
-                                            <li><a href="#">- Dropdown</a>
-                                                <ul class="dropdown">
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                    <li><a href="#">- Dropdown Item</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="./rent.html">Rent</a></li>
-                                    <li><a href="./agent.html">Agents</a></li>
-                                    <li><a href="./about.html">About</a></li>
-                                    <li><a href="./blog.html">Blog</a></li>
-                                </ul>
-
-                                <!-- Contact btn -->
-                                <div class="contact-btn mt-3 mt-lg-0 ml-3 ml-lg-5">
-                                    <a href="#">Contact Us</a>
-                                </div>
-                            </div>
-                            <!-- Nav End -->
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
+<?php
+include("header.html");
+?>
     <!-- **** Header Area End **** -->
 
     <!-- **** Breadcrumb Area Start **** -->
@@ -125,7 +11,7 @@
                     <div class="breadcrumb-content">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Contact</li>
                             </ol>
                         </nav>
@@ -211,48 +97,10 @@
     <!-- **** Contact Area End **** -->
 
     <!-- **** Google Maps **** -->
-    <div class="contact-maps mb-80 wow fadeInUp" data-wow-delay="200ms">
-        <div class="bottom-arrow"></div>
-        <!-- Maps Iframe -->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8148.712307515693!2d-75.44253062844327!3d43.77270348052156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d8ba8be82f81b3%3A0x5fa5f4eeadd52643!2sWatson%2C+NY%2C+USA!5e0!3m2!1sen!2sbd!4v1551758930043" allowfullscreen></iframe>
-    </div>
+
 
     <!-- **** Contact Information Start **** -->
-    <div class="contact--information-">
-        <div class="container">
-            <div class="row mb-40">
-                <!-- Contact Info Area -->
-                <div class="col-12 col-md-4">
-                    <div class="contact--info-area text-center wow fadeInUp" data-wow-delay="200ms">
-                        <h4>London</h4>
-                        <p>Add: <span>Iris Watson, Box 283 8562 Rd, NY</span></p>
-                        <p>Email: <span>info.colorlib@gmail.com</span></p>
-                        <p>Phone: <span>123-456-789</span></p>
-                    </div>
-                </div>
-
-                <!-- Contact Info Area -->
-                <div class="col-12 col-md-4">
-                    <div class="contact--info-area text-center wow fadeInUp" data-wow-delay="200ms">
-                        <h4>United State</h4>
-                        <p>Add: <span>Iris Watson, Box 283 8562 Rd, NY</span></p>
-                        <p>Email: <span>info.colorlib@gmail.com</span></p>
-                        <p>Phone: <span>123-456-789</span></p>
-                    </div>
-                </div>
-
-                <!-- Contact Info Area -->
-                <div class="col-12 col-md-4">
-                    <div class="contact--info-area text-center wow fadeInUp" data-wow-delay="200ms">
-                        <h4>Japan</h4>
-                        <p>Add: <span>Iris Watson, Box 283 8562 Rd, NY</span></p>
-                        <p>Email: <span>info.colorlib@gmail.com</span></p>
-                        <p>Phone: <span>123-456-789</span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <!-- **** Contact Information End **** -->
 
     <!-- **** Partner Area Start **** -->
@@ -310,8 +158,8 @@
 
                             <!-- Contact Area -->
                             <div class="footer-contact">
-                                <p>Phone: <span>(12) 345 6789</span></p>
-                                <p>Email: <span>colorlib.info@gmail.com</span></p>
+                                <p>Phone: <span>(+212) </span></p>
+                                <p>Email: <span>contact@ihainnovation.com</span></p>
                                 <p>Address: <span>Iris Watson, Box 283 8562 Fusce Rd, NY</span></p>
                             </div>
                         </div>
@@ -363,7 +211,7 @@
                     <div class="col-12 col-sm-6">
                         <div class="copywrite-text">
                             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made by <a href="https://ihainnovation.com" target="_blank">IHA INNOVATION</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                         </div>
                     </div>
