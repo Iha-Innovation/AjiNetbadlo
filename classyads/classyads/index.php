cbf79821a813801c6f8df202728314d53719c757
iha-innovation <iha-innovation@github.com>
2019-09-20 12:03:53 -0700

    acceuil temp1 FAQ

--- classyads/classyads/index.php
+++ classyads/classyads/index.php
@@ -453,38 +453,35 @@ Votre logement est loin du boulot ? Echanger votre maison et optez pour la proxi
       <div class="container">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Our Blog</h2>
-            <p class="color-black-opacity-5">See Our Daily News &amp; Updates</p>
+            <h2 class="font-weight-light text-primary">FAQ</h2>
+            <p class="color-black-opacity-5"> Frequently Asked Questions</p>
           </div>
         </div>
         <div class="row mb-3 align-items-stretch">
           <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
             <div class="h-entry">
-              <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-              <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-              <div class="meta mb-3">by Mark Spiker<span class="mx-1">&bullet;</span> Jan 18, 2019 <span class="mx-1">&bullet;</span> <a href="#">News</a></div>
+             
+              <h2 class="font-size-regular"><a href="#" class="text-black">Comment cela fonctionne ?</a></h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
             </div> 
           </div>
           <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
             <div class="h-entry">
-              <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-              <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-              <div class="meta mb-3">by Mark Spiker<span class="mx-1">&bullet;</span> Jan 18, 2019 <span class="mx-1">&bullet;</span> <a href="#">News</a></div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            
+              <h2 class="font-size-regular"><a href="#" class="text-black">Puis-je avoir confiance ?</a></h2>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
             </div> 
           </div>
           <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
             <div class="h-entry">
-              <img src="images/hero_1.jpg" alt="Image" class="img-fluid rounded">
-              <h2 class="font-size-regular"><a href="#" class="text-black">Many People Selling Online</a></h2>
-              <div class="meta mb-3">by Mark Spiker<span class="mx-1">&bullet;</span> Jan 18, 2019 <span class="mx-1">&bullet;</span> <a href="#">News</a></div>
+            
+              <h2 class="font-size-regular"><a href="#" class="text-black">Existe-t-il un contrat pour échanger sa maison ?</a></h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
             </div>
           </div>
 
           <div class="col-12 text-center mt-4">
-            <a href="#" class="btn btn-primary rounded py-2 px-4 text-white">View All Posts</a>
+            <a href="#" class="btn btn-primary rounded py-2 px-4 text-white">Autres questions</a>
           </div>
         </div>
       </div>
